9fd80446f418782ff13c7ffc51682a97d36b3de6
Nithil <nithillsethupathi@gmail.com>
2018-11-11 15:41:31 -0800

    Update JobScheduleTest.java

--- JobScheduleTest.java
+++ JobScheduleTest.java
@@ -30,4 +30,32 @@ public class JobScheduleTest{
         assertEquals(0, j.getJob(7).getStartTime());
         assertEquals(87, j.minCompletionTime());
     }
+	@Test
+    public void sampleTest()
+    {
+        JobSchedule schedule = new JobSchedule();
+        schedule.addJob(8);
+        JobSchedule.Job j1 = schedule.addJob(3);
+        schedule.addJob(5);
+        assertEquals(8, schedule.minCompletionTime()); //should return 8, since job 0 takes time 8 to complete.
+         //Note it is not the min completion time of any job, but the earliest the entire set can complete. 
+        schedule.getJob(0).requires(schedule.getJob(2));
+        assertEquals(13, schedule.minCompletionTime()); //should return 13 (job 0 cannot start until time 5)
+        schedule.getJob(0).requires(j1); //job 1 must precede job 0
+        assertEquals(13, schedule.minCompletionTime()); //should return 13
+        assertEquals(5, schedule.getJob(0).getStartTime());
+        assertEquals(0, j1.getStartTime());
+        assertEquals(0, schedule.getJob(2).getStartTime());
+        j1.requires(schedule.getJob(2));
+        assertEquals(16, schedule.minCompletionTime()); //should return 16
+        schedule.getJob(0).getStartTime();
+        schedule.getJob(1).getStartTime();
+        schedule.getJob(2).getStartTime();
+        schedule.getJob(1).requires(schedule.getJob(0));
+        assertEquals(-1,schedule.minCompletionTime());
+        assertEquals(-1, schedule.getJob(0).getStartTime());
+        assertEquals(-1, schedule.getJob(1).getStartTime());
+        assertEquals(0,schedule.getJob(2).getStartTime());
+    }
+}
 }
