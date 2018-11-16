1cd705b6ce698a713e57975a2ccb73292e2310a1
Nithil <nithillsethupathi@gmail.com>
2018-11-16 14:26:37 -0800

    detects cycle in small graphs

--- JobSchedule.java
+++ JobSchedule.java
@@ -2,13 +2,13 @@ import java.util.*;
 public class JobSchedule{
 
 	private ArrayList<Job> jobs;          				
-	private Boolean sorting;							
+	//private Boolean sorting;							
 	private Boolean cycle;						
 	private int minTime;							
 
 	public JobSchedule(){
 		jobs = new ArrayList<>();						//An ArrayList to add the vertices that are being tested
-		sorting = true;									//Assign sorting to be true by default	
+		//sorting = true;									//Assign sorting to be true by default	
 		cycle = false;									//Should be acyclic, so keep it by default
 		minTime = 0;									//Initializing the minimum time   
 	}
@@ -24,8 +24,7 @@ public class JobSchedule{
 	}
 
 	public int minCompletionTime(){
-		if(sorting)
-			topologicalSort();							//Goes to the topologicalSort method if sorting is needed
+		topologicalSort();							//Goes to the topologicalSort method if sorting is needed
 		if(cycle)                       								
 			return -1;									//Terminates the program if there is a cycle
 		return minTime;									//Returns the minimum completion time taken.
@@ -39,8 +38,11 @@ public class JobSchedule{
 				topOrder(j, stack);						//If j not discovered, goes to topOrder method
 			}
 		}
+		minTime = 0;
+
 		while(!stack.empty()){
 			Job j = stack.pop();						//Empty stack by popping
+			j.startTime = 0;
 			if(!(j.inDegree == 0)){						//If inDegree is not zero, set startTime to minimum completion time until that vertex
 				j.startTime = minTime;	
 				minTime = minTime + j.time;				//Add job time to minimum completion time
@@ -51,20 +53,31 @@ public class JobSchedule{
 				}
 			}
 		}
-		sorting = false;								//When sorting is done, set sorting flag to false
+		//sorting = false;								//When sorting is done, set sorting flag to false
 	}
 	public void topOrder(Job j, Stack<Job> stack){   	
-		j.discovered = true;							//Set discovered to true.
+		j.discovering = true;							//Set discovering to true.
 		for(Job j1: j.outgoingEdges){					//Goes to all adjacent vertices
-			if(!j1.discovered)
-				topOrder(j1, stack);					//Calls topOrder on adjacent vertices depth first
+			if(!j1.discovered){
+				if(!j1.discovering){
+					topOrder(j1, stack);
+				}
+				if(j1.discovering){
+					j1.jobCycle = true;
+					cycle = true;
+					return;
+				}				
+			}
 		}
+		j.discovered = true;
+		j.discovering = false;
 		stack.push(j);									//Add the object to the stack
 	}
 
 	public void resetGraph(){
 		for(Job j: jobs){
 			j.discovered = false;						//Resets all the discovered vertices to undiscovered
+			j.discovering = false;
 		}
 	}
 
@@ -74,26 +87,29 @@ public class JobSchedule{
 		public int startTime;							//startTime of the job
 		public int inDegree;							//Number of incoming edges to the vertex					 
 		public ArrayList<Job> outgoingEdges;			//ArrayList of outgoingEdges from the Job
+		public Boolean jobCycle;
 		public Boolean discovered;						//Vertex that have been discovered
+		public Boolean discovering;
 		public Job(int time){
 			this.time = time;							//Initializing the Job time
 			startTime = 0;								//Set the startTime to 0.
+			jobCycle = false;
 			inDegree = 0;								//Set the inDegree to 0.
 			outgoingEdges = new ArrayList<>();			//Create a new ArrayList of outgoingEdges
 		}
 		public void requires(Job j){
-			sorting = true;								//Set sorting flag to 0 by default.
+		//	sorting = true;								//Set sorting flag to 0 by default.
 			this.inDegree++;								//Adding inDegree whenever there is an edge pointing to object j.
 			j.outgoingEdges.add(this);					//Adding outgoing edges from one vertex to an other vertex
 		}
 
 		public int getStartTime(){
-			if(sorting){
-				topologicalSort();						//If sorting is true, goes to topologicalSort method
+			topologicalSort();						//If sorting is true, goes to topologicalSort method
+			if(jobCycle){
+				return -1;
 			}
 			return this.startTime;						//Returns the startTime of the object.
 		}
 
 	}
 }
-
