eccd6d4d7eaf2037843583343907441bfbc2c2fe
Nithil Sethupathi <nithillsethupathi@gmail.com>
2018-11-16 16:05:39 -0800

    new test

--- JobScheduleTest.java
+++ JobScheduleTest.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+
+import jdk.internal.jline.internal.TestAccessible;
+
 import static org.junit.Assert.*;
 
 public class JobScheduleTest{
@@ -12,7 +15,7 @@ public class JobScheduleTest{
         j.addJob(9);	//4
         j.addJob(11);	//5
         j.addJob(15);	//6
-        j.addJob(19);	//7
+        j.addJob(19);	//7        
         j.getJob(1).requires(j.getJob(0));
         j.getJob(2).requires(j.getJob(1));
         j.getJob(3).requires(j.getJob(2));
@@ -70,5 +73,40 @@ public class JobScheduleTest{
 		js.getJob(2).requires(js.getJob(1));
 		assertEquals(-1,js.getJob(1).getStartTime());
 		assertEquals(0,js.getJob(3).getStartTime());
-	}
+    }
+    @Test
+    public void test2(){
+        schedule = new JobSchedule();
+        schedule.addJob(5);
+        schedule.addJob(2);
+        schedule.addJob(10);
+        schedule.addJob(12);
+        schedule.getJob(1).requires(schedule.getJob(0));
+        schedule.getJob(2).requires(schedule.getJob(1));
+        schedule.getJob(3).requires(schedule.getJob(0));
+        schedule.getJob(1).requires(schedule.getJob(3));
+        assertEquals(29, schedule.minCompletionTime());
+        assertEquals(0, schedule.getJob(0).getStartTime());
+        assertEquals(17, schedule.getJob(1).getStartTime());
+        assertEquals(19, schedule.getJob(2).getStartTime());
+        assertEquals(5, schedule.getJob(3).getStartTime());
+        schedule.addJob(100);
+        schedule.addJob(10);
+        schedule.getJob(5).requires(schedule.getJob(4));
+        assertEquals(110, schedule.minCompletionTime());
+        assertEquals(0, schedule.getJob(4).getStartTime());
+        assertEquals(100, schedule.getJob(5).getStartTime());
+        schedule.getJob(2).requires(schedule.getJob(5));
+        assertEquals(120, schedule.minCompletionTime());
+        assertEquals(110, schedule.getJob(2).getStartTime());
+        assertEquals(110, schedule.getJob(2).getStartTime());
+        assertEquals(100, schedule.getJob(5).getStartTime());
+        schedule.addJob(7);
+        schedule.getJob(6).requires(schedule.getJob(3));
+        assertEquals(17, schedule.getJob(6).getStartTime());
+        schedule.getJob(3).requires(schedule.getJob(6));
+        assertEquals(-1, schedule.minCompletionTime());
+        assertEquals(100, schedule.getJob(5).getStartTime());
+        assertEquals(-1, schedule.getJob(2).getStartTime());
+    }
 }
